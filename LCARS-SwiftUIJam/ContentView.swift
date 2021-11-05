5fc8185ffb423733d7be6175cb36745ec1dcb8d7
Fraser Kuyvenhoven <fkuyvenhoven@marketcircle.com>
2021-11-05 11:01:36 -0400

    Initial Commit

---
+++ LCARS-SwiftUIJam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LCARS-SwiftUIJam
+//
+//  Created by Fraser Kuyvenhoven on 2021-11-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LCARS-SwiftUIJam/LCARS_SwiftUIJamApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCARS_SwiftUIJamApp.swift
+//  LCARS-SwiftUIJam
+//
+//  Created by Fraser Kuyvenhoven on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct LCARS_SwiftUIJamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
