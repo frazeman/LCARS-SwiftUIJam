f7890c1c0e69816d4ade9d76fe0a633dba41b25c
Sebastien Boisvert <sebastienboisvert@users.noreply.github.com>
2021-11-07 13:05:19 -0500

    Some layout adjustments

--- LCARS-SwiftUIJam/AuxSystemsMonitorHeader.swift
+++ LCARS-SwiftUIJam/AuxSystemsMonitorHeader.swift
@@ -10,33 +10,36 @@ import SwiftUI
 struct AuxSystemsMonitorHeader: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-        HStack(alignment: .top, spacing: 0) {
-            VStack(alignment: .leading, spacing: 0) {
-                RectangleGen(color: .orange, title: "LCARS 23295", alignment: .bottomTrailing)
-                RectangleGen(color: Color("LightPurple"), title: "01-23584", alignment: .topTrailing)
+            HStack(alignment: .top, spacing: 0) {
+                VStack(alignment: .leading, spacing: 5) {
+                    RectangleGen(color: .orange, title: "LCARS 23295", alignment: .bottomTrailing)
+                    RectangleGen(color: Color("LightPurple"), title: "01-23584", alignment: .topTrailing)
 
-            } // Main Header Left Section
-            VStack(alignment: .trailing) {
-                Text("AUXILIARY SYSTEMS MONITOR 882")
-                    .font(Font.LCARS.title)
-                    .foregroundColor(.orange)                    .multilineTextAlignment(.trailing)
-                HStack  {
-                    AnimatedGridView()
-                        .scaledToFit()
-                    VStack(alignment: .trailing) {
-                        CapsuleButtons(color: Color("LightPurple"), title: "07-3215")
-                        CapsuleButtons(color: Color("LightPink"), title: "09-2548")
-                    } // Button Stack 1
-                    VStack(alignment: .trailing) {
-                        CapsuleButtons(color: Color("LightOrange"), title: "QUIT")
-                        CapsuleButtons(color: Color("LightOrange"), title: "10-6215")
-                    } // Button Stack 2
-                } // Main Headers Data Section
-                
-            } // Main Header Right Section
-        
-        } // Main Header Section
-            HStack(alignment: .bottom, spacing: 0) { 
+                } // Main Header Left Section
+                Spacer()
+                VStack(alignment: .trailing) {
+                        VStack(alignment: .trailing) {
+                            Text("AUXILIARY SYSTEMS MONITOR 882")
+                                .font(Font.LCARS.title)
+                                .foregroundColor(.orange)                    .multilineTextAlignment(.trailing)
+
+                            HStack  {
+                                AnimatedGridView()
+                                //                        .scaledToFill()
+                                VStack(alignment: .trailing) {
+                                    CapsuleButtons(color: Color("LightPurple"), title: "07-3215")
+                                    CapsuleButtons(color: Color("LightPink"), title: "09-2548")
+                                } // Button Stack 1
+                                VStack(alignment: .trailing) {
+                                    CapsuleButtons(color: Color("LightOrange"), title: "QUIT")
+                                    CapsuleButtons(color: Color("LightOrange"), title: "10-6215")
+                                } // Button Stack 2
+                            } // Main Headers Data Section
+                        }
+                }// Main Header Right Section
+
+            } // Main Header Section
+            HStack(alignment: .bottom, spacing: 0) {
                 Corner(columnWidth: 180, bannerHeight: 15,
                        color: Color("LightPurple"), orientation: .bottomLeft)
                 Rectangle().fill(Color("LightPurple"))
@@ -44,21 +47,24 @@ struct AuxSystemsMonitorHeader: View {
             }
 
         }
-        .padding()
-            .frame(
-                minWidth: .zero,
-                maxWidth: .infinity,
-                minHeight: .zero,
-                maxHeight: .infinity
-            )
-            .background(Color.black)
+        .frame(
+            minWidth: .zero,
+            maxWidth: .infinity,
+            minHeight: .zero,
+            maxHeight: .infinity
+        )
+        .background(Color.black)
     }
 }
 
 struct AuxSystemsMonitorHeader_Previews: PreviewProvider {
     static var previews: some View {
-        AuxSystemsMonitorHeader()
-            .previewInterfaceOrientation(.landscapeRight)
+        Group {
+            AuxSystemsMonitorHeader()
+                .previewInterfaceOrientation(.portraitUpsideDown)
+            AuxSystemsMonitorHeader()
+                .previewInterfaceOrientation(.landscapeLeft)
+        }
 
     }
 }
