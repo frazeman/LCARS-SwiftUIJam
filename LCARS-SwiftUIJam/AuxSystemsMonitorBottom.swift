d841d2dbacf177c22d0e387b1a9b5da33197361b
Fraser Kuyvenhoven <fkuyvenhoven@marketcircle.com>
2021-11-07 17:19:45 -0500

    Removed debug border

--- LCARS-SwiftUIJam/AuxSystemsMonitorBottom.swift
+++ LCARS-SwiftUIJam/AuxSystemsMonitorBottom.swift
@@ -39,7 +39,6 @@ struct AuxSystemsMonitorBottom: View {
 
                 AnimatedGridView(animationStyle: .fullscreen)
                     .padding(.leading, 20)
-                    .border(Color.white)
 
             } // bottom content (below banner)
         }
