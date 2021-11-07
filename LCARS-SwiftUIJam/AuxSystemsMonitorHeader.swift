8b7cd0e0f1a2703bdc627c9cdabfca09762b8a18
Sebastien Boisvert <sebastienboisvert@users.noreply.github.com>
2021-11-06 23:26:56 -0400

    Started filling in the AuxSystemsMonitorHeader

--- LCARS-SwiftUIJam/AuxSystemsMonitorHeader.swift
+++ LCARS-SwiftUIJam/AuxSystemsMonitorHeader.swift
@@ -9,23 +9,25 @@ import SwiftUI
 
 struct AuxSystemsMonitorHeader: View {
     var body: some View {
-        HStack {
-            VStack {
+        VStack(alignment: .leading, spacing: 0) {
+        HStack(alignment: .top, spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
                 RectangleGen(color: .orange, title: "LCARS 23295", alignment: .bottomTrailing)
                 RectangleGen(color: Color("LightPurple"), title: "01-23584", alignment: .topTrailing)
+
             } // Main Header Left Section
             VStack(alignment: .trailing) {
                 Text("AUXILIARY SYSTEMS MONITOR 882")
                     .font(Font.LCARS.title)
                     .foregroundColor(.orange)                    .multilineTextAlignment(.trailing)
-                HStack {
-                    Text("Fraser's data goes here")
-                        .foregroundColor(.blue)
-                    VStack {
+                HStack  {
+                    AnimatedGridView()
+                        .scaledToFit()
+                    VStack(alignment: .trailing) {
                         CapsuleButtons(color: Color("LightPurple"), title: "07-3215")
                         CapsuleButtons(color: Color("LightPink"), title: "09-2548")
                     } // Button Stack 1
-                    VStack {
+                    VStack(alignment: .trailing) {
                         CapsuleButtons(color: Color("LightOrange"), title: "QUIT")
                         CapsuleButtons(color: Color("LightOrange"), title: "10-6215")
                     } // Button Stack 2
@@ -34,6 +36,14 @@ struct AuxSystemsMonitorHeader: View {
             } // Main Header Right Section
         
         } // Main Header Section
+            HStack(alignment: .bottom, spacing: 0) { 
+                Corner(columnWidth: 180, bannerHeight: 15,
+                       color: Color("LightPurple"), orientation: .bottomLeft)
+                Rectangle().fill(Color("LightPurple"))
+                    .frame(width: nil, height: 15, alignment: .bottom)
+            }
+
+        }
         .padding()
             .frame(
                 minWidth: .zero,
@@ -48,5 +58,7 @@ struct AuxSystemsMonitorHeader: View {
 struct AuxSystemsMonitorHeader_Previews: PreviewProvider {
     static var previews: some View {
         AuxSystemsMonitorHeader()
+            .previewInterfaceOrientation(.landscapeRight)
+
     }
 }
