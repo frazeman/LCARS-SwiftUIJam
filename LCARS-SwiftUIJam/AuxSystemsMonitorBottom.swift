c5a64edeb76ce80f6e125da9ce3f16fa6533d676
Sebastien Boisvert <sebastienboisvert@users.noreply.github.com>
2021-11-07 16:32:21 -0500

    Added new AuxSystemsMonitorBottom, updated main view

---
+++ LCARS-SwiftUIJam/AuxSystemsMonitorBottom.swift
@@ -0,0 +1,63 @@
+//
+//  AuxSystemsMonitorBottom.swift
+//  LCARS-SwiftUIJam
+//
+//  Created by Sebastien Boisvert on 2021-11-07.
+//
+
+import SwiftUI
+
+struct AuxSystemsMonitorBottom: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(alignment: .top, spacing: 0) {
+
+                    let bannerHeight = 22.0
+
+                    Corner(columnWidth: 180, bannerHeight: bannerHeight,
+                           color: .brown, orientation: .topLeft)
+                    Rectangle().fill(.brown)
+                        .frame(width: nil, height: bannerHeight, alignment: .topLeading)
+                    HStack(alignment: .bottom, spacing: 5) {
+                        RectangleGen(color: Color("RosePink"), height: bannerHeight, width: 50)
+                        Rectangle().fill(.orange).frame(width: 150, height: bannerHeight/2, alignment: .topLeading) // TODO: needs to hug top!
+                        RectangleGen(color: Color("LightPink"),height: bannerHeight, width: 250)
+                        RectangleGen(color: Color("RosePink"), height: bannerHeight, width: 50)
+                    } // Top corner bar
+                    .padding(EdgeInsets(top: 0.0, leading: 5.0, bottom: 0.0, trailing: 0.0))
+                }
+                RectangleGen(color: Color(.brown), title: "02-24156", height: 70, width: 180, alignment: .bottomTrailing)
+            } // Top banner
+            HStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    RectangleGen(color: Color(.brown), title: "03-41248", height: 170, width: 180, alignment: .bottomTrailing)
+                    RectangleGen(color: Color(.orange), title: "04-14702", height: 60, width: 180, alignment: .bottomTrailing)
+                    RectangleGen(color: Color(.orange), title: "04-14702", height: .infinity, width: 180, alignment: .topTrailing)
+                } // Left Sidebar
+                
+                Text("Number grid")
+                    .foregroundColor(.blue)
+                    .frame(width: .infinity, height: .infinity, alignment: .center)
+
+            } // bottom content (below banner)
+        }
+        .frame(minWidth: .zero,
+               idealWidth: nil,
+               maxWidth: .infinity)
+        .background(Color.black)
+    }
+}
+
+struct AuxSystemsMonitorBottom_Previews: PreviewProvider {
+    static var previews: some View {
+        AuxSystemsMonitorBottom()
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
+
+struct AuxSystemsMonitorBottom_Previews_2: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
--- LCARS-SwiftUIJam/AuxSystemsMonitorMain.swift
+++ LCARS-SwiftUIJam/AuxSystemsMonitorMain.swift
@@ -11,32 +11,23 @@ struct AuxSystemsMonitorMain: View {
     var body: some View {
     
         HStack {
-            VStack {
-            
-                HStack {
-                    AuxSystemsMonitorHeader()
-                }
-                HStack {
-                    Corner(columnWidth: 0.5, bannerHeight: 50, color: Color("LightPink"), orientation: .bottomLeft)
-                        RectangleGen(color: Color("RosePink"), height: 50, width: 400)
-                        RectangleGen(color: .orange, height: 50, width: 50)
-                        RectangleGen(color: Color("LightPink"),height: 50, width: 200)
-                        RectangleGen(color: Color("LightPink"),height: 50, width: 400)
-                        RectangleGen(color: Color("RosePink"), height: 50, width: 100)
-                }
-            }
-            VStack {
-            
-                
-            }
+            AuxSystemsMonitorHeader()
+//            AuxSystemsMonitorBottom()
         }
+        .frame(minWidth: .zero,
+               idealWidth: .infinity,
+               maxWidth: .infinity,
+               minHeight: .zero,
+               idealHeight: .infinity,
+               maxHeight: .infinity,
+               alignment: .top)
+        .background(.black)
     }
-    
 }
 
 struct AuxSystemsMonitorMain_Previews: PreviewProvider {
     static var previews: some View {
         AuxSystemsMonitorMain()
-.previewInterfaceOrientation(.landscapeRight)
+.previewInterfaceOrientation(.landscapeLeft)
     }
 }
